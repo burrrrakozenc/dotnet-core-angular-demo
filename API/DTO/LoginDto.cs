e38ed4aaf29f1c7ef41b860f46419219fc5a72df
burrrrakozenc <brk.ozenc@gmail.com>
2021-03-15 01:20:04 +0300

    end of section5

---
+++ API/DTO/LoginDto.cs
@@ -0,0 +1,10 @@
+namespace API.DTO
+{
+    public class LoginDto
+    {
+        // [Required]
+        public string Username { get; set; }
+        // [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ API/Interfaces/ITokenService.cs
@@ -0,0 +1,19 @@
+using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateToken(AppUser user);
+    }
+}
+
+// using API.Entities;
+
+// namespace API.Interfaces
+// {
+//     public interface ITokenService
+//     {
+        
+//     }
+// }
